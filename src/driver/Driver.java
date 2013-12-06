9142ffce65419bccec64d347f28457bfd97813fd
loganperreault <perreault.logan@gmail.com>
2013-12-06 13:58:24 -0700

    Removed constants (stop words).
    Signed-off-by: loganperreault <perreault.logan@gmail.com>

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -1,13 +1,16 @@
 package driver;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import text.*;
-
 import kldivergence.KLDivergence;
+import kldivergence.Results;
+import text.*;
 
 public class Driver {
 	
@@ -15,6 +18,7 @@ public class Driver {
 	private static List<Predicate> localPredicates = new ArrayList<Predicate>();
 	// the list of predicates that have been matched by algorithm
 	private static List<Predicate> remotePredicates = new ArrayList<Predicate>();
+	static List<Results> results;
 	
 	public static void main(String [] args) {
 	
@@ -24,21 +28,84 @@ public class Driver {
 		// create the KL Divergence class
 		KLDivergence kld = new KLDivergence(compare);
 		// set the max number of predicates to look at
-		kld.setLimit(500);
+		kld.setLimit(1000);
 		// set the starting threshold for the string comparison (will be decreased dynamically if too strict)
-		kld.setStartingThreshold(0.91);
-//		kld.setStartingThreshold(0.60);
+//		kld.setStartingThreshold(0.85);
+		kld.setStartingThreshold(0.70);
+		
+		kld.setThresholdStep(0.01);
+		kld.setValidRequired(5);
+		
+		// test 1: limit 500, JW compare, starting .996, step .002, required 6
+		// test 4: limit 500, DICE compare, starting .85, step .02, required 5
+		
+		// test A: limit 800, JW compare, starting .95, step .02, required 10
+		// test B: limit 800, JW compare, starting .95, step .02, required 5
+		// test C: limit 800, JW compare, starting .95, step .02, required 15
+		// test D: limit 800, JW compare, starting .95, step .02, required 3
+		// test E: limit 3000, JW compare, starting .85, step .01, required 1
+		
 		
 		// choose which predicates will be matched
-//		testMoviesSubset();
-		testSpeciesSubset();
+		testMoviesSubset();
+//		testSpeciesSubset();
 		
-		//kld.setEcho(true);	// prints additional information
+		stripConstants();
+		
+//		kld.setEcho(true);	// prints additional information
 		
 		// execute the algorithm, matching predicates as well as possible
 		Map<Predicate, Predicate> matches = kld.select(localPredicates, remotePredicates);
+		results = kld.getResults();
 		System.out.println();
 		
+		normalizeResults(results);
+		
+		printResults(results);
+//		saveResults(results, "results/");
+		
+	}
+	
+	private static void stripConstants() {
+		for (Predicate predicate : localPredicates) {
+			predicate.stripConstants();
+		}
+		for (Predicate predicate : remotePredicates) {
+			predicate.stripConstants();
+		}
+	}
+	
+	private static void normalizeResults(List<Results> results) {
+		for (Results result : results) {
+			result.normalize();
+		}
+	}
+	
+	private static void printResults(List<Results> results) {
+		for (Results result : results) {
+			System.out.println(result);
+		}
+	}
+	
+	private static void saveResults(List<Results> results, String directory) {
+		FileWriter fw;
+		BufferedWriter bw;
+		for (Results result : results) {
+			String fileName = directory + result.getName();
+			System.out.println("Writing "+fileName);
+			File file = new File(fileName);
+			try {
+				fw = new FileWriter(file.getAbsoluteFile());
+				bw = new BufferedWriter(fw);
+				bw.write(result.toString());
+				bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void printExactMatches(Map<Predicate, Predicate> matches) {
 		// loop through list of predicates that were searched for in DB2
 		for (Predicate predicate : localPredicates) {
 			// retrieves the match returned by the algorithm
@@ -47,8 +114,6 @@ public class Driver {
 			System.out.print(match.getConfidence()+": ");
 			System.out.println(predicate.getShortName() + " ~ "+ match.getShortName());
 		}
-		
-		
 	}
 	
 	// read a subset of dbpedia predicates and all taxonomy predicates
@@ -56,12 +121,12 @@ public class Driver {
 		String local_directory = "data/species/dbpedia/";
 		String target_directory = "data/species/taxonomy/";
 		
-		localPredicates.add(new Predicate(local_directory+"http.dbpedia.org.ontology.class"));
-		localPredicates.add(new Predicate(local_directory+"http.dbpedia.org.ontology.family"));
+//		localPredicates.add(new Predicate(local_directory+"http.dbpedia.org.ontology.class"));
+//		localPredicates.add(new Predicate(local_directory+"http.dbpedia.org.ontology.family"));
 		localPredicates.add(new Predicate(local_directory+"http.dbpedia.org.ontology.genus"));
-		localPredicates.add(new Predicate(local_directory+"http.dbpedia.org.ontology.kingdom"));
-		localPredicates.add(new Predicate(local_directory+"http.dbpedia.org.ontology.order"));
-		localPredicates.add(new Predicate(local_directory+"http.dbpedia.org.ontology.phylum"));
+//		localPredicates.add(new Predicate(local_directory+"http.dbpedia.org.ontology.kingdom"));
+//		localPredicates.add(new Predicate(local_directory+"http.dbpedia.org.ontology.order"));
+//		localPredicates.add(new Predicate(local_directory+"http.dbpedia.org.ontology.phylum"));
 		
 		remotePredicates = getPredicatesFromDirectory(target_directory);
 	}
@@ -71,18 +136,16 @@ public class Driver {
 		String dbpedia_directory = "data/movies/dbpedia/";
 		String lmdb_directory = "data/movies/linkedmdb/";
 		
-		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.starring"));
+//		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.starring"));
 		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.director"));
-		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.producer"));
-		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.writer"));
-		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.editor"));
-		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.cinematography"));
-		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.genre"));
-		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.precededBy"));
-		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.followedBy"));
-		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.language"));
-		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.country"));
-		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.rating"));
+//		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.producer"));
+//		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.writer"));
+//		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.editor"));
+//		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.genre"));
+//		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.precededBy"));
+//		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.followedBy"));
+//		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.country"));
+//		localPredicates.add(new Predicate(dbpedia_directory+"http.dbpedia.org.property.rating"));
 		
 		remotePredicates = getPredicatesFromDirectory(lmdb_directory);
 	}
--- src/driver/Predicate.java
+++ src/driver/Predicate.java
@@ -5,148 +5,188 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class Predicate {
         
-        // the name of the file the predicate reads from
-        public String filename;
-        // the name of the predicate, using the name of the file only (no directory)
-        public String name;
-        // the confidence value associated with matched predicates from an algorithm
-        private double confidence = -1.0;
-        // a map relating a word (line of text) to the number of times that word appears
-        private Map<String, Integer> words = new HashMap<String, Integer>();
-        // an iterator used for looping through the words in the predicate
-        Iterator<Entry<String, Integer>> it;
-        // an arrayList for getting entries in the predicate
-        ArrayList<Map.Entry<String, Integer>> list = null;
-        
-        // Predicates with no file are blank
-        public Predicate() {
-                name = ".NULL";
+    // the name of the file the predicate reads from
+    public String filename;
+    // the name of the predicate, using the name of the file only (no directory)
+    public String name;
+    // the confidence value associated with matched predicates from an algorithm
+    private double confidence = -1.0;
+    // a map relating a word (line of text) to the number of times that word appears
+    private Map<String, Integer> words = new HashMap<String, Integer>();
+    // an iterator used for looping through the words in the predicate
+    Iterator<Entry<String, Integer>> it;
+    // an arrayList for getting entries in the predicate
+    ArrayList<Map.Entry<String, Integer>> list = null;
+    
+    // Predicates with no file are blank
+    public Predicate() {
+        name = ".NULL";
+    }
+    
+    // creates a predicate tied to a file
+    public Predicate(String filename) {
+        this.filename = filename;
+        populate();
+    }
+    
+    // populates the predicate's word list by reading the filename
+    public void populate() {
+        try {
+            read(filename);
+        } catch (IOException e) {        System.out.println("Failed to read file: "+filename);        }
+    }
+    
+    // reads and parses the file, adding word counts to the internal list
+    private void read(String filename) throws IOException {
+        // store filename information
+        File file = new File(filename);
+        name = file.getName();
+        // create reader for file
+        BufferedReader in = new BufferedReader(new FileReader(file));
+        String str = null;
+
+        // loop through file line by line
+        while ((str = in.readLine()) != null) {
+        	// count the number of times the word has been seen
+        	if (words.containsKey(str))
+                words.put(str, words.get(str) + 1);
+        	else
+                words.put(str, 1);
         }
-        
-        // creates a predicate tied to a file
-        public Predicate(String filename) {
-                this.filename = filename;
-                populate();
-        }
-        
-        // populates the predicate's word list by reading the filename
-        public void populate() {
-                try {
-                        read(filename);
-                } catch (IOException e) {        System.out.println("Failed to read file: "+filename);        }
-        }
-        
-        // reads and parses the file, adding word counts to the internal list
-        private void read(String filename) throws IOException {
-                // store filename information
-                File file = new File(filename);
-                name = file.getName();
-                // create reader for file
-                BufferedReader in = new BufferedReader(new FileReader(file));
-                String str = null;
-        
-                // loop through file line by line
-                while ((str = in.readLine()) != null) {
-                	// count the number of times the word has been seen
-                	if (words.containsKey(str))
-                        words.put(str, words.get(str) + 1);
-                	else
-                        words.put(str, 1);
-                }
-                // cleanup
-                in.close();
-                words = new TreeMap<String, Integer>(words);
-                it = words.entrySet().iterator();
-        }
-        
-        // retrieve the first word-count pair in the list
-        public Map.Entry<String, Integer> getStart() {
-                it = words.entrySet().iterator();
-                return getNext();
-        }
-        
-        // retrieve the next word-count pair in the list
-        public Map.Entry<String, Integer> getNext() {
-                if (it.hasNext())
-                        return it.next();
-                else
-                        return null;
-        }
-        
-        public ArrayList<Map.Entry<String, Integer>> getEntries() {
-        	list = new ArrayList();
-        	list.addAll(words.entrySet());
-        	return list;
-        }
-        
-        // retrieve the terminator for the word-count pairs in the list
-        public Map.Entry<String, Integer> getLast() {
-                return null;
-        }
-        
-        // return the name of the predicate
-        public String toString() {
-                return name;
-        }
-        
-        // return the substring of the name after the last '.' token
-        // NOTE: this may and does result in duplicate shortNames for a database.
-        public String getShortName() {
-                return name.substring(name.lastIndexOf('.')+1);
-        }
-        
-        
-        /**
-         * Retrieve the number of times a word appears in a predicate.
-         *
-         * @param key        The word to count.
-         * @return                The number of times the word appears.
-         */
-        public int getCount(String key) {
-                if (words.containsKey(key))
-                        return words.get(key);
-                else
-                        return 0;
-        }
-        
-        /**
-         * Calculates the probability that a word is chosen from the predicate at random.
-         *
-         * @param key        The word to get the probability for.
-         * @return                The probability within the range [0.0,1.0].
-         */
-        public double getProbability(String key) {
-                return ((double)getCount(key) / size());
-        }
-        
-        /**
-         * @return        The number of distinct words in the predicate.
-         */
-        public int size() {
-                return words.size();
-        }
-        
-        // sets the confidence level for how well the match
-        public void setConfidence(double confidence) {
-                this.confidence = confidence;
-        }
-        
-        // retrieve the confidence associated with a matched predicate
-        public double getConfidence() {
-                return this.confidence;
-        }
-        
-        // clear the list of words to save space (may read from file again if necessary)
-        public void free() {
-                words.clear();
-        }
+        // cleanup
+        in.close();
+        words = new TreeMap<String, Integer>(words);
+        it = words.entrySet().iterator();
+    }
+    
+    // retrieve the first word-count pair in the list
+    public Map.Entry<String, Integer> getStart() {
+        it = words.entrySet().iterator();
+        return getNext();
+    }
+    
+    // retrieve the next word-count pair in the list
+    public Map.Entry<String, Integer> getNext() {
+        if (it.hasNext())
+            return it.next();
+        else
+            return null;
+    }
+    
+    public ArrayList<Map.Entry<String, Integer>> getEntries() {
+    	list = new ArrayList();
+    	list.addAll(words.entrySet());
+    	return list;
+    }
+    
+    // retrieve the terminator for the word-count pairs in the list
+    public Map.Entry<String, Integer> getLast() {
+        return null;
+    }
+    
+    // return the name of the predicate
+    public String toString() {
+        return name;
+    }
+    
+    // return the substring of the name after the last '.' token
+    // NOTE: this may and does result in duplicate shortNames for a database.
+    public String getShortName() {
+        return name.substring(name.lastIndexOf('.')+1);
+    }
+    
+    
+    /**
+     * Retrieve the number of times a word appears in a predicate.
+     *
+     * @param key        The word to count.
+     * @return                The number of times the word appears.
+     */
+    public int getCount(String key) {
+        if (words.containsKey(key))
+            return words.get(key);
+        else
+            return 0;
+    }
+    
+    /**
+     * Calculates the probability that a word is chosen from the predicate at random.
+     *
+     * @param key        The word to get the probability for.
+     * @return                The probability within the range [0.0,1.0].
+     */
+    public double getProbability(String key) {
+        return ((double)getCount(key) / size());
+    }
+    
+    /**
+     * @return        The number of distinct words in the predicate.
+     */
+    public int size() {
+        return words.size();
+    }
+    
+    // sets the confidence level for how well the match
+    public void setConfidence(double confidence) {
+        this.confidence = confidence;
+    }
+    
+    // retrieve the confidence associated with a matched predicate
+    public double getConfidence() {
+        return this.confidence;
+    }
+    
+    // clear the list of words to save space (may read from file again if necessary)
+    public void free() {
+        words.clear();
+    }
+    
+    public void stripConstants() {
+    	Iterator<Entry<String, Integer>> it = words.entrySet().iterator();
+    	Map.Entry<String, Integer> entry = null;
+    	List<String> toRemove = null;
+    	for (entry = it.next(); it.hasNext(); entry = it.next()) {
+    		List<String> tokens = new ArrayList<String>(Arrays.asList(entry.getKey().split("\\s+")));
+    		if (toRemove == null) {
+				toRemove = new ArrayList<String>();
+				for (String token : tokens) {
+	    			toRemove.add(token);
+	    		}
+			} else {
+				for (int i = 0; i < toRemove.size(); i++) {
+					String removeItem = toRemove.get(i);
+					if (!tokens.contains(removeItem)) {
+						toRemove.remove(i);
+					}
+				}
+			}
+    	}
+    	
+    	Map<String, Integer> newWords = new HashMap<String, Integer>();
+    	it = words.entrySet().iterator();
+    	for (entry = it.next(); it.hasNext(); entry = it.next()) {
+    		String key = entry.getKey();
+    		Integer value = entry.getValue();
+    		// remove constants
+    		for (String removeItem : toRemove) {
+    			key = key.replace(removeItem, "");
+    		}
+    		newWords.put(key, value);
+    	}
+    	
+    	// set list to newly constructed list
+    	words = newWords;
+    	
+    }
 
 }
